ab82f19ff909dda13c34e1ea15dac32193cb7acb
Tanmoy Samanta <nix93826@gmail.com>
2026-07-19 19:02:01 +0530

    Update dartA2ZbasicsPart1.dart

--- dartA2ZbasicsPart1.dart
+++ dartA2ZbasicsPart1.dart
@@ -3,6 +3,20 @@
 
 import 'dart:io';
 
+
+
+class Num {
+  int num = 10;
+}
+
+int notEqual() {
+  var n = Num();
+  int number220;
+  number220 = n.num ?? 0;
+  print(number220);
+  return 0;
+}
+
 void main() {
 
   print("Hello World");
@@ -107,4 +121,104 @@ void main() {
 // != not Equal
 if num16!= 100{
   print('num is not equal to 100');
+
+
+   var n = Num();
+  int number220;
+  number220 = n.num ?? 0;
+  print(number220);
+  return 0;
 }
+
+void main() {
+  notEqual();
+
+  // Null Aware Operator
+  int? number16;
+  print(number16 ??= 100);
+  print(number16);
+
+  // Ternary Operator
+  int x11 = 100;
+  var result122 = x11 % 2 == 0 ? 'Even' : 'Odd';
+  print(result122);
+
+  // Type test
+  var x123 = 100;
+  if (x123 is int) {
+    print('Integer');
+  }
+
+  // Conditional Statement
+  int number124 = 100;
+  if (number124 % 2 == 0) {
+    print('Even');
+  } else if (number124 % 2 != 0) {
+    print('Odd');
+  } else {
+    print("Confused");
+  }
+
+  // Switch Statement
+  int number134 = 0;
+  switch (number134) { 
+    case 0:
+      print('Even');
+      break;
+    case 1: // lowercase 'case'
+      print('Odd');
+      break;
+    default:
+      print('Confused');
+  }
+
+  // For-in Loop
+  var number146 = [1, 2, 3];
+  for (var n in number146) { 
+    print(n);
+  }
+
+  // While loop
+  int number148 = 5;
+  while (number148 > 0) {
+    print(number148);
+    number148 -= 1;
+  }
+
+  // Break and Continue
+  for (var i = 0; i < 10; ++i) {
+    if (i > 5) break; // Outof the loop
+    print(i);
+  }
+
+  for (var i = 0; i < 10; ++i) {
+    if (i % 2 == 0) continue; // Inside the loop
+    print("odd: $i"); 
+  }
+}
+  // List
+  List<String> names149 = ['Jack', 'jaill'];
+  print(names149);
+  for (var n in names149) {
+    print(n);
+  }
+
+  // Collection 
+  List<String> name151 = ['hello', 'Fiends'];
+  // Create a new list
+  var names152 = [...name151]; 
+  names152[1] = 'Mark';
+  for (var n in names152) {
+    print(n);
+  }
+
+  // Map
+  // Use Map<KeyType, ValueType>() constructor or literal
+  var gift221 = <String, String>{}; 
+  print(gift221);
+  gift221['first'] = 'Mego';
+  print(gift221['first']); 
+  var gift222 = {'first': 'Mango', 'second': 'Jack Friend'};
+  print(gift222['second']);
+}
+
